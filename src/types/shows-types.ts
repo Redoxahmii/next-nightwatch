9ff203dd527db13c6b1fac914b1c987c6f8dff3a
Redoxahmii <fullmetal2334@gmail.com>
2024-10-14 08:22:48 +0500

    feat(types): add types for shows and json for conversions

---
+++ src/types/shows-types.ts
@@ -0,0 +1,20 @@
+export interface Shows {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  origin_country: string[];
+  original_language: string;
+  original_name: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  first_air_date: Date;
+  name: string;
+  vote_average: number;
+  vote_count: number;
+  navigateLink: string;
+  posterPath: string;
+  embedUrl: string;
+  trailerUrl: string;
+}
